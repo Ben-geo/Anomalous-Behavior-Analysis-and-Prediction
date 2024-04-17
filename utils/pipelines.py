92ca5680992bcdecefac946cb209942c24e4889e
Ben-geo <bengeoabraham@gmail.com>
2024-04-17 12:13:13 +0530

    Cleaning completed created eda

---
+++ utils/pipelines.py
@@ -0,0 +1,9 @@
+import pyspark.sql.functions as F
+from pyspark.sql.functions import isnan, when, count, col
+
+def count_missings(df,sort=True):
+    """
+    Counts number of nulls and nans in each column
+    """
+    df.select([count(when(isnan(c), c)).alias(c) for c in df.columns]).show()
+    df.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in df.columns]).show()
