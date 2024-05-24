16f58d0f5319f18d1ecb15553ef3e9444ee4b271
Ben-geo <bengeoabraham@gmail.com>
2024-05-24 20:07:05 +0530

    Complete Feature Engineering

--- utils/feature_engineering.py
+++ utils/feature_engineering.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.express as px
 import polars as pl
 
+import ppscore as pps
 
 def aggregate_node_features(
     data:pl.DataFrame,node_features:list[str],by:str="_id"    
@@ -27,3 +28,70 @@ def aggregate_node_features(
         std = pl.col(f).std().alias(f"std_{f}")
         aggs += [avg, min_val, max_val, std]
     return data.group_by(by).agg(aggs)
+
+def feature_predictive_power(
+    data: pl.DataFrame, x: str, y: str, plot: bool = True
+) -> np.float32:
+    """Utility to calcualte predictive power of a feature and plot its relationship with the target
+    Args:
+        data (pl.DataFrame): input dataframe
+        x (str): name of the feature
+        y (str): name of the target
+        plot (bool, optional): indicator whether you want to plot the relationship. Defaults to True.
+
+    Returns:
+        np.float32: predictive power score
+    """
+    data_pd = data.select([x, y]).to_pandas()
+    score = np.float32(pps.score(data_pd, x, y)["ppscore"]).round(4)
+
+    if plot:
+        print(f"Predictive Power Score: {score}")
+        fig = px.histogram(
+            x=data_pd[x],
+            color=data_pd[y],
+            marginal="box",
+            histnorm="probability",
+            title=f"{x} distribution by {y}",
+        )
+        fig.show()
+
+    return score
+
+def get_graph_features(data: pl.DataFrame, node_features: bool = True) -> pl.DataFrame:
+    """Pipeline function to generate graph features
+
+    Args:
+        data (pl.DataFrame): dataframe with edges 'from' and 'to'
+        node_features (bool, optional): Indicator whether you want to create node level features. Defaults to True.
+
+    Returns:
+        pl.DataFrame: dataframe with engineered features
+    """
+    graph_features = (
+        data.groupby("_id")
+        .agg(pl.count().alias("n_connections"), pl.col("from"), pl.col("to"))
+        .with_columns(
+            pl.concat_list("from", "to")
+            .list.unique()
+            .list.lengths()
+            .alias("n_unique_nodes")
+        )
+        .select(["_id", "n_connections", "n_unique_nodes"])
+    )
+
+    if node_features:
+        node_features_agg = aggregate_node_features(
+            data,
+            node_features=[
+                "global_source_degrees",
+                "global_dest_degrees",
+                "local_source_degrees",
+                "local_dest_degrees",
+            ],
+            by="_id",
+        )
+
+        graph_features = graph_features.join(node_features_agg, on="_id")
+
+    return graph_features
